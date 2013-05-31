40a955edefd2676bbbadcf56c13019bf5511b458
Gavin Mannings <g.mannings@hotmail.co.uk>
2013-05-31 16:42:38 +0100

    Adjusted to allow CSS em height elements and for animation when resized

--- jquery.equalSizeElements.js
+++ jquery.equalSizeElements.js
@@ -8,8 +8,11 @@
     $.fn.equalSizeElements = function(dimension) {
 
         var
+            instance = this,
             intLargestSize = 0,  // The largest element size is kept in this variable
-            strDimension;
+            newHeight,
+            strDimension,
+            sizeOfEm = _getDefaultFontSize();
 
         // Dimension parameters
         if (!dimension || dimension === 'height') {
@@ -21,13 +24,73 @@
         }
 
         // Make all elements in group the same height as largest element
-        return this.each(function() {
-                    if ($(this).height() > intLargestSize) {
-                        intLargestSize = $(this).height();
-                    }
-                }).each(function() {
-                    $(this).css(strDimension, intLargestSize)
-                });
+        return this
+            .each(function() {
+
+                if ($(this).height() > intLargestSize) {
+                    // Save the height
+                    intLargestSize = $(this).height();
+
+                    // Apply a unique class to the largest element
+                    $(instance).removeClass('resizeLargestItem');
+                    $(this).addClass('resizeLargestItem');
+                }
+
+            })
+            .filter(function() {
+
+                // If we're applying to height we want to use 'em' measurements instead of px
+                // for accessibility
+                var
+                    animationObj = {},
+                    newSize = (strDimension === 'height') ?
+                        String(intLargestSize / sizeOfEm.height) + 'em' :
+                        intLargestSize + 'px';
+
+                // Create the animation object to pass to jQuery
+                animationObj[strDimension] =  String(newSize);
+
+                // Animate and if we need ems as the final measurement, apply
+                $(instance)
+                    .animate(animationObj, 200, function() {
+                        $(this).css(strDimension, newSize);
+                    });
+
+                return this;
+            });
+
+        /**
+         * Returns the width and height of one em
+         * @param el
+         * @returns {{width: number, height: number}}
+         * @private
+         */
+        function _getDefaultFontSize(el){
+
+            var
+                parentEl = el || document.body,
+                sizeOfOneCssEm,
+                tempEl = document.createElement('div');
+
+            // Hide the element from view but still get the height of 1em
+            tempEl.style.cssText='display:inline-block; padding:0; line-height:1; ' +
+                'position:absolute; visibility:hidden; font-size:1em';
+
+            // Append the element to the DOM tree
+            tempEl.appendChild(document.createTextNode('M'));
+            parentEl.appendChild(tempEl);
+
+            // Get the size of one em
+            sizeOfOneCssEm = {
+                width : tempEl.offsetWidth,
+                height: tempEl.offsetHeight
+            };
+
+            // Clean up
+            parentEl.removeChild(tempEl);
+
+            return sizeOfOneCssEm;
+        }
 
     };
 })( jQuery );
